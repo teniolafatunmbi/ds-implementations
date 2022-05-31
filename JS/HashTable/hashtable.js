3da1505baad4112deae80d0a024b5fd5f29c7724
devteni <fatunmbiteniola001@gmail.com>
2022-05-31 07:17:51 +0100

    restructuring

---
+++ JS/HashTable/hashtable.js
@@ -0,0 +1,21 @@
+class HashTable {
+    constructor(size){
+      this.data = new Array(size);
+    }
+  
+    _hash(key) {
+      let hash = 0;
+      for (let i =0; i < key.length; i++){
+          hash = (hash + key.charCodeAt(i) * i) % this.data.length
+      }
+      return hash;
+    }
+}
+
+// create the get and set functions to add and retrieve key, value data from the hash table.
+  
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000)
+myHashTable.get('grapes')
+myHashTable.set('apples', 9)
+myHashTable.get('apples')
