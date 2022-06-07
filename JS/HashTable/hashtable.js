1a5a7b8d7187d59c8bba48bb3b4d5e3d88a34692
devteni <fatunmbiteniola001@gmail.com>
2022-06-07 16:14:23 +0100

    feat(): implemented hashtables and solved FRC

--- JS/HashTable/hashtable.js
+++ JS/HashTable/hashtable.js
@@ -3,6 +3,7 @@ class HashTable {
       this.data = new Array(size);
     }
   
+    // really fast in real-life scenarios -> O(1)
     _hash(key) {
       let hash = 0;
       for (let i =0; i < key.length; i++){
@@ -10,12 +11,50 @@ class HashTable {
       }
       return hash;
     }
+
+    // 0(1)
+    set(key, value) {
+      let memAddress = this._hash(key);
+      if (!this.data[memAddress]){
+        this.data[memAddress] = [];
+      }
+      this.data[memAddress].push([key,value]);
+      return this.data;
+    }
+
+    // O(1), in cases of hash collisions -> O(n)
+    get(key) {
+      let memAddress = this._hash(key);
+      const currentBucket = this.data[memAddress];
+      if (currentBucket) {
+        for (let i=0; i < currentBucket.length; i++) {
+          if (currentBucket[i][0] === key) {
+            console.log(currentBucket[i][1]);
+            return currentBucket[i]
+          }
+        }
+      }
+      return undefined;
+    }
+
+    keys() {
+      const entries = [];
+      if (this.data) {
+        for (let i=0; i < this.data.length; i++) {
+          if (this.data[i]) {
+            entries.push(this.data[i][0][0]);
+          }
+        }
+      }
+      console.log(entries);
+      return entries;
+    }
 }
 
 // create the get and set functions to add and retrieve key, value data from the hash table.
   
 const myHashTable = new HashTable(50);
-myHashTable.set('grapes', 10000)
-myHashTable.get('grapes')
-myHashTable.set('apples', 9)
-myHashTable.get('apples')
+myHashTable.set('grapes', 10000);
+myHashTable.set('apples', 8);
+myHashTable.set('oranges', 3);
+myHashTable.keys();
