8818f02a2400d1eefde1ec438403c5b8ae5c4c63
devteni <fatunmbiteniola001@gmail.com>
2022-09-09 07:17:41 +0100

    :sparkles: created boilerplate for stack implementation

---
+++ JS/Stack/Stack.js
@@ -0,0 +1,33 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null
+    }
+}
+
+
+class Stack {
+    constructor() {
+        this.top = null;
+        this.bottom = null;
+        this.length = 0;
+    }
+
+    peek() {
+        //
+    }
+
+    push(value) {
+        //
+    }
+
+    pop() {
+        //
+    }
+
+    isEmpty() {
+        //
+    }
+}
+
+const myStack = new Stack()
